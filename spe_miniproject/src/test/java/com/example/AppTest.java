fae837303bfe052a8eaf80ec8d302bc4f6a6aa52
hemc <hemanthx9@gmail.com>
2022-04-10 19:12:33 +0530

    Testing added

--- spe_miniproject/src/test/java/com/example/AppTest.java
+++ spe_miniproject/src/test/java/com/example/AppTest.java
@@ -1,17 +1,25 @@
 package com.example;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import calc.scientific_calculator.Calculator;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    Calculator calculator = new Calculator();
+    private double delta = 1e-10;
+
+    @Test
+    public void testFactorialtruePositive()
+    {
+        assertEquals(24, calculator.factorial(4), delta);
+    }
     @Test
     public void shouldAnswerWithTrue()
     {
