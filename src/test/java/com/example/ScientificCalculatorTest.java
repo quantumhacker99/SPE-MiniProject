cfdadb38ce424d0ddb6d5ee7ee83cd659f5bc6fa
hemc <hemanthx9@gmail.com>
2022-04-13 18:03:02 +0530

    Added tests

--- src/test/java/com/example/ScientificCalculatorTest.java
+++ src/test/java/com/example/ScientificCalculatorTest.java
@@ -8,15 +8,21 @@ import org.junit.Test;
 
 import calc.scientific_calculator.ScientificCalculator;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
+public class ScientificCalculatorTest{
     private double delta = 1e-10;
 
     @Test
-    public void testFactorialtruePositive()
+    public void testSqrtTruePositive(){
+        assertEquals(2, ScientificCalculator.sqrt(4), delta);
+    }
+
+    @Test
+    public void testSqrtFalsePositive(){
+        assertNotEquals(3, ScientificCalculator.sqrt(4),delta);
+    }
+
+    @Test
+    public void testFactorialTruePositive()
     {
         assertEquals(24, ScientificCalculator.factorial(4), delta);
     }
@@ -25,6 +31,29 @@ public class AppTest
     public void testFactorialFalsePositive(){
         assertNotEquals(30, ScientificCalculator.factorial(4),delta);
     }
+
+    @Test
+    public void testLnTruePositive()
+    {
+        assertEquals(1, ScientificCalculator.ln(Math.E), delta);
+    }
+
+    @Test
+    public void testLnFalsePositive(){
+        assertNotEquals(3, ScientificCalculator.ln(Math.E),delta);
+    }
+
+    @Test
+    public void testPowTruePositive()
+    {
+        assertEquals(20.25, ScientificCalculator.pow(4.5,2), delta);
+    }
+
+    @Test
+    public void testPowFalsePositive(){
+        assertNotEquals(20, ScientificCalculator.pow(4.5,2),delta);
+    }
+
 
     @Test
     public void shouldAnswerWithTrue()
